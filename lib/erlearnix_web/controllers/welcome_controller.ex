ba6515a1378e0dc0147856262afc03439618be88
Daniel Lima <daniel.lima@r2u.io>
2021-02-27 15:05:18 -0300

    sum numbers

--- lib/erlearnix_web/controllers/welcome_controller.ex
+++ lib/erlearnix_web/controllers/welcome_controller.ex
@@ -1,7 +1,25 @@
 defmodule ErlearnixWeb.WelcomeController do
   use ErlearnixWeb, :controller
+  alias Erlearnix.Numbers
 
-  def index(conn, _params) do
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+
     text(conn, "Welcome to the Elixir API")
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> IO.inspect()
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to the Elixir API. Here is your number #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(reason)
   end
 end
